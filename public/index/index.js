29881aa92e50a4ff3f813763941ceb5abd9f904a
Andreas Rømer <andreasrmr@hotmail.com>
2020-12-29 22:21:30 +0100

    tilføjet jwt

--- public/index/index.js
+++ public/index/index.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
     $("#home").click();
 });
 
+/*
 $("#about").click(function (event) {
     const url = "/about";
     event.preventDefault();
     $(".content").load(url);
 });
+*/
 
 $("#home").click(function(event) {
     const url = "/home";
@@ -45,3 +47,68 @@ $("#logout").on("submit", function(event) {
     $(".notifications").html("You logged out").css("color", "red");
 });
 
+$('#about').click(function (e){
+    const url = '/about';
+    e.preventDefault(); 
+    getAuthPage(url); 
+});
+
+async function getAuthPage(url) {
+    //renew accessToken if expired.
+    if($.cookie('accessToken') == null) {
+        const urlToken = '/auth/token';
+        const refreshToken = $.cookie("refreshToken");
+        const userId = $.cookie("userId");
+        const data = {
+            "token": refreshToken,
+            "userId": userId
+        }
+        await $.ajax({
+            type: 'POST',
+            url: urlToken,
+            data: data,
+            ContentType: "application/json",
+            success : function(data){
+                $('.notifications').html(data);
+            },
+            error : function(data){
+                $('.notifications').html(data.responseText);
+            }
+        });        
+    }
+    await $.ajax({
+        url: url,
+        type:'GET',
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader ("Authorization", "Bearer " + $.cookie("accessToken"));      
+        },
+        success : function(data){
+            $(".content").html(data);
+        },
+        error : function(data){
+            $(".notifications").html(data.responseText);        
+        }
+    });
+}
+
+async function updateAccessToken() {
+    const url = '/auth/token';
+    const refreshToken = $.cookie("refreshToken");
+    const userId = $.cookie("userId");
+    const data = {
+        "token": refreshToken,
+        "userId": userId
+    }
+    await $.ajax({
+        type: 'POST',
+        url: url,
+        data: data,
+        ContentType: "application/json",
+        success : function(data){
+            $('.notifications').html(data);
+        },
+        error : function(data){
+            $('.notifications').html(data.responseText);
+        }
+    });
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const mysql = require('mysql2/promise');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const pool = mysql.createPool({
     host        : process.env.DB_HOST,
@@ -13,6 +14,17 @@ const pool = mysql.createPool({
     queueLimit          : 0
 });
 
+//cookie optionas
+const accessTokenOptions = {
+    maxAge: 5000, //30 sekunder bør sættes op
+    httpOnly: false 
+}
+
+const refreshTokenOptions = {
+    maxAge: 15000, //120 sekunder bør sættes op
+    httpOnly: false 
+}
+
 router.post("/auth/login", async (req, res) => {
     try {
         const email = req.body.email;
@@ -21,10 +33,28 @@ router.post("/auth/login", async (req, res) => {
         if(rows[0] === undefined || rows[0].length == 0) {
             return res.status(403).send("Username incorrect");
         };
+        const userId = rows[0].id;
         const hashedPassword = rows[0].password;
         const plainTextPassword = req.body.password;
         const isCorrectPW = await bcrypt.compare(plainTextPassword, hashedPassword);        
         if(isCorrectPW){
+            //user authenticated here.
+            const user = { name: email };
+            const accessToken = generateAccessToken(user);
+            const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+            
+            //delete old refreshToken if it exists
+            await pool.execute('DELETE FROM refresh_tokens WHERE id = ?', [userId]);
+            
+            //store new refreshToken in db
+            await pool.execute('INSERT INTO refresh_tokens SET id = ?, token = ?', [userId, refreshToken]);
+            
+            //Store cookies in browser   
+            res.cookie("accessToken", accessToken, accessTokenOptions);
+            res.cookie("refreshToken", refreshToken, refreshTokenOptions);
+            res.cookie("userId", userId);
+            res.cookie("email", email);
+        
             return res.send("Your logged in");
         }
         else {
@@ -35,6 +65,8 @@ router.post("/auth/login", async (req, res) => {
         return res.status(500).send(err);
     }
 });
+
+
 
 router.post("/auth/register", async (req, res) => {
     try {
@@ -48,6 +80,35 @@ router.post("/auth/register", async (req, res) => {
         return res.status(500).send(err);
     }
 });
+
+//User can renew access access token with their refreshToken.
+router.post("/auth/token", async (req, res) => {
+    const refreshToken = req.body.token;
+    const userId = req.body.userId;
+    if(refreshToken == null) {
+        return res.status(401).send("Please login"); 
+    }
+    const storedRefreshToken = await pool.execute('SELECT token FROM refresh_tokens WHERE id = ?', [userId]);
+    //check if users given refreshToken exists in db
+    if(!storedRefreshToken[0][0].token === refreshToken){
+        return res.status(403).send("Refresh token is not working. Try Logging in again.");
+    };
+    //very og generer ny access token.
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if(err) {
+            return res.status(403).send("refreshToken is not available / valid. Please register / login again");
+        };
+        const accessToken = generateAccessToken({ name: user.name })
+        //save cookie
+        res.cookie("accessToken", accessToken, accessTokenOptions);       
+        res.send('new access token set.')
+    })
+});
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '120s' })
+}
+
 
 module.exports = router;
 
